c325abeb5008a44b417c949e69979fb0e16a65f2
munxw_ <momunov.kr@gmail.com>
2022-12-26 15:12:18 +0600

    .

---
+++ main.js
@@ -0,0 +1,4 @@
+let button = document.querySelectorAll(">btn");
+let button1 = document.querySelectorAll(">btn");
+let button2 = document.querySelectorAll(">btn");
+let button3 = document.querySelectorAll(">btn");
