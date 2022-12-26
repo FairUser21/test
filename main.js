64f3be02b810931de3a270a3e626fea53fe2bb8a
FairUser21 <renat.alimbekov2002@gmail.com>
2022-12-26 15:10:46 +0600

    .

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log(1);
+
+let input = document.querySelector(".text");
+let input1 = document.querySelector(".text1");
+let input2 = document.querySelector(".text2");
+let input3 = document.querySelector(".text3");
+let input4 = document.querySelector(".text4");
